d4c6fe861d762fd49a53c6be2029fca8d008c9b7
Mehmet Baran Özdeniz <Ozdenizm6@gmail.com>
2024-06-04 20:43:17 +0300

    Api ve Controller işlevleri tanımlandı

--- src/main/java/com/amazon/asset/AssetApplication.java
+++ src/main/java/com/amazon/asset/AssetApplication.java
@@ -2,8 +2,10 @@ package com.amazon.asset;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AssetApplication {
 
 	public static void main(String[] args) {
